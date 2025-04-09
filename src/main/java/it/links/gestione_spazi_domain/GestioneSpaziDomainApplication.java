cef9018214638ddb38b53595da604c5e25d32809
Luigi Albino <luigialbino@hotmail.it>
2025-04-09 11:49:33 +0200

    importo progetto

---
+++ src/main/java/it/links/gestione_spazi_domain/GestioneSpaziDomainApplication.java
@@ -0,0 +1,13 @@
+package it.links.gestione_spazi_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneSpaziDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneSpaziDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/links/gestione_spazi_domain/GestioneSpaziDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package it.links.gestione_spazi_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneSpaziDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
